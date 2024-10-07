c11421a75c8bc0e3c8e8bb721fbac151a4eaf101
Carlos Castillo <cookieo9@gmail.com>
2024-10-07 16:24:33 -0700

    refactor(xiter): export Process
    The internal tool that was used to build Map, Filter and the like is good enough to be exported and used by others.
    
    No tests, as the current users already test it extensively.

--- xiter/map.go
+++ xiter/map.go
@@ -5,7 +5,7 @@ import "iter"
 // Map applies the given function f to each element of the input iterator it,
 // and returns a new iterator that yields the results of applying f.
 func Map[T, U any](it iter.Seq[T], f func(T) U) iter.Seq[U] {
-	return process(it, func(t T, yield func(U) bool) bool {
+	return Process(it, func(t T, yield func(U) bool) bool {
 		return yield(f(t))
 	})
 }
