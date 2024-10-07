99c7e8f46340a61a12b01b1b159cf1a61a3a59e6
Carlos Castillo <cookieo9@gmail.com>
2024-10-07 12:22:44 -0700

    feat(xiter): add Filter and Exclude
    Filter returns a new iterator only containing the items matching a
    predicate. Exclude inverts the meaning of the predicate, omitting
    values where the predicate is true.

---
+++ xiter/filter_test.go
@@ -0,0 +1,51 @@
+package xiter
+
+import (
+	"testing"
+)
+
+func filterTestCase[T any](name string, source []T, want []T, f func(T) bool) GenericTestCase {
+	return IteratorCollectTest(name, Filter(sliceValues(source), f), want)
+}
+
+func excludeTestCase[T any](name string, source []T, want []T, f func(T) bool) GenericTestCase {
+	return IteratorCollectTest(name, Exclude(sliceValues(source), f), want)
+}
+
+func TestFilterExclude(t *testing.T) {
+	nums := []int{1, 2, 3, 4, 5}
+	isEven := func(i int) bool { return i%2 == 0 }
+	all := func(i int) bool { return true }
+
+	t.Run("Filter", func(t *testing.T) {
+		GenericTestCases{
+			filterTestCase("all", nums, nums, all),
+			filterTestCase("allEmpty", []int{}, []int{}, all),
+			filterTestCase("allNil", nil, nil, all),
+
+			filterTestCase("isEven", nums, []int{2, 4}, isEven),
+			filterTestCase("isEvenEmpty", []int{}, []int{}, isEven),
+			filterTestCase("isEvenNil", nil, nil, isEven),
+
+			PanicTestCases(func(s func(func(int) bool)) func(func(int) bool) {
+				return Filter(s, func(i int) bool { return true })
+			}),
+		}.Run(t)
+	})
+
+	t.Run("Exclude", func(t *testing.T) {
+		GenericTestCases{
+			excludeTestCase("all", nums, []int{}, all),
+			excludeTestCase("allEmpty", []int{}, []int{}, all),
+			excludeTestCase("allNil", nil, nil, all),
+
+			excludeTestCase("isEven", nums, []int{1, 3, 5}, isEven),
+			excludeTestCase("isEvenEmpty", []int{}, []int{}, isEven),
+			excludeTestCase("isEvenNil", nil, nil, isEven),
+
+			PanicTestCases(func(s func(func(int) bool)) func(func(int) bool) {
+				return Exclude(s, func(i int) bool { return false })
+			}),
+		}.Run(t)
+	})
+}
