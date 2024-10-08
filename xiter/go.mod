f142d7d0a9696391d19d555959eb60e4d4075a9c
Carlos Castillo <cookieo9@gmail.com>
2024-10-07 18:05:31 -0700

    feat: add xiter/pipeline package
    This package will provide utilities for creating and evaluating pipelines of iterator conversions, such that at the end one will have an iterator generating the desired sequence.

--- xiter/go.mod
+++ xiter/go.mod
@@ -2,7 +2,10 @@ module github.com/cookieo9/go-std-addons/xiter
 
 go 1.23.2
 
-require github.com/stretchr/testify v1.9.0
+require (
+	github.com/cookieo9/go-std-addons v0.0.2
+	github.com/stretchr/testify v1.9.0
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
---
+++ xiter/pipe/pipe_test.go
@@ -0,0 +1,23 @@
+package pipeline
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPipeline(t *testing.T) {
+	data := []int{1, 2, 3, 4, 5}
+	want := []float64{1.1, 3.3}
+	isOdd := func(x int) bool { return x%2 == 1 }
+
+	got, err := ProcessSlice[float64](data, Pipeline(
+		Filter(isOdd),
+		Map(func(x int) float64 { return float64(x) * 1.1 }),
+		While(func(f float64) bool { return f < 4 }),
+		Materialize[float64](),
+	))
+	require.NoError(t, err)
+	assert.InEpsilonSlice(t, want, got, 0.0000001, "same sequence")
+}
