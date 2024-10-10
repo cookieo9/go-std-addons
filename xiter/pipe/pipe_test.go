b3be21e861b8feb8428e706a28d12ba17f9fca3c
Carlos Castillo <cookieo9@gmail.com>
2024-10-10 14:14:42 -0700

    refactor(xiter/pipe): make test non-local
    Change the package for the test to pipe_test so that it has to import and
    use the package as a user would have to, both to check the package rename
    and to see the ergonomics of using it in the wild.

--- xiter/pipe/pipe_test.go
+++ xiter/pipe/pipe_test.go
@@ -1,10 +1,12 @@
-package pipe
+package pipe_test
 
 import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
+	"github.com/cookieo9/go-std-addons/xiter/pipe"
 )
 
 func TestPipeline(t *testing.T) {
@@ -12,11 +14,11 @@ func TestPipeline(t *testing.T) {
 	want := []float64{1.1, 3.3}
 	isOdd := func(x int) bool { return x%2 == 1 }
 
-	got, err := ProcessSlice[float64](data, Pipeline(
-		Filter(isOdd),
-		Map(func(x int) float64 { return float64(x) * 1.1 }),
-		While(func(f float64) bool { return f < 4 }),
-		Materialize[float64](),
+	got, err := pipe.ProcessSlice[float64](data, pipe.Pipeline(
+		pipe.Filter(isOdd),
+		pipe.Map(func(x int) float64 { return float64(x) * 1.1 }),
+		pipe.While(func(f float64) bool { return f < 4 }),
+		pipe.Materialize[float64](),
 	))
 	require.NoError(t, err)
 	assert.InEpsilonSlice(t, want, got, 0.0000001, "same sequence")
