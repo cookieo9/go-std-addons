3ddfba16c59666c1c57e3c53873ea7abcf345657
Carlos Castillo <cookieo9@gmail.com>
2024-10-10 17:24:11 -0700

    refactor(xiter/pipe): cleanups for pipelines
    Serveral fixes to make working with pipelines more ergonomic:
     - rename Pipeline to Join
       - this hides the pipeline type further
       - also created TryJoin variant that returns error on failed
         validation
       - passing a single item to join simply returns it
       - Join can be used collapse vararg lists of processors into a single
         processor automatically and efficently
     - alter Process to panic on failure to build pipeline, not return error
     - alter Process to use Join to collaps varargs into a processor
     - alter ProcessSlice to take vararg processors via Process
     - alter ProcessSlice to preserve Process's panic, but still catch error
       while producing slice.
     - update existing test to use new semantics
     - add new test for various results of TryJoin/Join

--- xiter/pipe/pipe_test.go
+++ xiter/pipe/pipe_test.go
@@ -14,12 +14,52 @@ func TestPipeline(t *testing.T) {
 	want := []float64{1.1, 3.3}
 	isOdd := func(x int) bool { return x%2 == 1 }
 
-	got, err := pipe.ProcessSlice[float64](data, pipe.Pipeline(
+	got, err := pipe.ProcessSlice[float64](data,
 		pipe.Filter(isOdd),
 		pipe.Map(func(x int) float64 { return float64(x) * 1.1 }),
 		pipe.While(func(f float64) bool { return f < 4 }),
 		pipe.Materialize[float64](),
-	))
+	)
 	require.NoError(t, err)
 	assert.InEpsilonSlice(t, want, got, 0.0000001, "same sequence")
+}
+
+func TestJoins(t *testing.T) {
+	double := pipe.Map[int](func(x int) int { return x * 2 })
+	odd := pipe.Filter(func(x int) bool { return x%2 == 1 })
+	halfFloat := pipe.Map(func(x float64) float64 { return float64(x) / 2 })
+	testCases := []struct {
+		name         string
+		stages       []pipe.Processor
+		joinErr      assert.ErrorAssertionFunc
+		panicProcess bool
+	}{
+		{name: "no stages"},
+		{name: "double", stages: []pipe.Processor{double}},
+		{name: "odd, double", stages: []pipe.Processor{odd, double}},
+		{name: "double, halfFloat", stages: []pipe.Processor{double, halfFloat}, joinErr: assert.Error},
+		{name: "halfFloat, double", stages: []pipe.Processor{halfFloat, double}, joinErr: assert.Error},
+		{name: "halfFloat", stages: []pipe.Processor{halfFloat}, panicProcess: true},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			p, err := pipe.TryJoin(tc.stages...)
+			if tc.joinErr != nil {
+				tc.joinErr(t, err)
+				return
+			} else {
+				require.NoError(t, err)
+			}
+			if tc.panicProcess {
+				require.Panics(t, func() {
+					pipe.ProcessSlice[int]([]int{1, 2, 3}, p)
+				})
+				return
+			}
+			require.NotPanics(t, func() {
+				pipe.ProcessSlice[int]([]int{1, 2, 3}, p)
+			})
+		})
+	}
 }
