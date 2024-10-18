919a13545d1897b815895b5b8763f86df6e23b7f
Carlos Castillo <cookieo9@gmail.com>
2024-10-17 21:47:00 -0700

    Update unique_test.go
    Based on suggested fix, add a case for all unique values to ensure none are dropped.

--- xiter/unique_test.go
+++ xiter/unique_test.go
@@ -11,5 +11,6 @@ func TestUnique(t *testing.T) {
 		SliceCollectTest("empty", Unique(slices.Values([]int{})), nil),
 		SliceCollectTest("pidigits", Unique(slices.Values(piDigits)), []int{3, 1, 4, 5, 9, 2, 6}),
 		SliceCollectTest("pidigits-lim4", Limit(Unique(slices.Values(piDigits)), 4), []int{3, 1, 4, 5}),
+		SliceCollectTest("all-unique", Unique(slices.Values([]int{1, 2, 3, 4, 5})), []int{1, 2, 3, 4, 5}),
 	}.Run(t)
 }
