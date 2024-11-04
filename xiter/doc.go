7a0260a35b984bd328dcddaca5337f1d127d43dc
Carlos Castillo <cookieo9@gmail.com>
2024-11-04 07:50:37 -0800

    docs(xiter): add pacakge documentation

---
+++ xiter/doc.go
@@ -0,0 +1,4 @@
+// Package xiter provides functions that create iterators from common cases,
+// process an iterator to produce a new one, or consume an iterator in a
+// common or useful way.
+package xiter
