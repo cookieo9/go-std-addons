e547c6f243cff8427000381890630e3a9b15535f
Carlos Castillo <cookieo9@gmail.com>
2024-12-16 02:41:42 -0800

    feat(xiter): improve Map functions
    Add some more Map* conversion functions to support:
     - MapOut: convert from single iterator to dual iterator
     - MapIn: convert from dual iterator into single iterator
    
    Also convert Map to directly do it's thing instead of using Process. Although Process is useful to have, it's not needed to implement much of xiter itself.

--- xiter/map.go
+++ xiter/map.go
@@ -2,10 +2,38 @@ package xiter
 
 import "iter"
 
-// Map applies the given function f to each element of the input iterator it,
-// and returns a new iterator that yields the results of applying f.
+// Map applies the given function f to each element of the input iterator, and
+// yields the result of each to the output iterator.
 func Map[T, U any](it iter.Seq[T], f func(T) U) iter.Seq[U] {
-	return Process(it, func(t T, yield func(U) bool) bool {
-		return yield(f(t))
-	})
+	return func(yield func(U) bool) {
+		for t := range it {
+			if !yield(f(t)) {
+				return
+			}
+		}
+	}
+}
+
+// MapOut applies the given function f to each element of the input iterator,
+// producing two values that are yielded by the output iterator.
+func MapOut[T, V, W any](it iter.Seq[T], f func(T) (V, W)) iter.Seq2[V, W] {
+	return func(yield func(V, W) bool) {
+		for t := range it {
+			if !yield(f(t)) {
+				return
+			}
+		}
+	}
+}
+
+// MapIn applies the given function f to each pair of elements from the input
+// iterator, producing a single value that is yielded by the output iterator.
+func MapIn[T, U, V any](it iter.Seq2[T, U], f func(T, U) V) iter.Seq[V] {
+	return func(yield func(V) bool) {
+		for t, u := range it {
+			if !yield(f(t, u)) {
+				return
+			}
+		}
+	}
 }
